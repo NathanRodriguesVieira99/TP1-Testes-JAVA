133754e39031907ac974c58b1a6025b40a7d3288
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-05 12:04:39 -0300

    test(AppTest): update test class documentation

--- tp1-testes/src/test/java/com/tp1/AppTest.java
+++ tp1-testes/src/test/java/com/tp1/AppTest.java
@@ -1,16 +1,13 @@
 package com.tp1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
- * Unit test for simple App.
+ * Classe de testes para o App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
     void testApp() {
         assertEquals(1, 1);
