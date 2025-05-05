27846c9279988aa22204fc17273d7d264863d9e9
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-05 12:04:06 -0300

    refactor(ScientificCalculator): change methods to public and update documentation

--- tp1-testes/src/main/java/com/tp1/ScientificCalculator.java
+++ tp1-testes/src/main/java/com/tp1/ScientificCalculator.java
@@ -1,68 +1,128 @@
 package com.tp1;
 
+/**
+ * Classe responsável por operações matemáticas.
+ */
 public class ScientificCalculator {
-    // método de Soma
-    private double add(double a, double b) {
+
+    /** constructor. */
+    public ScientificCalculator() {
+
+    }
+
+    // ------------------- Operações Aritméticas -------------------
+
+    /**
+     * Soma dois números.
+     *
+     * @param a primeiro número
+     * @param b segundo número
+     * @return soma de a e b
+     */
+    public double add(double a, double b) {
         return a + b;
     }
 
-    // método de Subitração
-    private double subtract(double a, double b) {
+    /**
+     * Subtrai dois números.
+     *
+     * @param a primeiro número
+     * @param b segundo número
+     * @return subtração de a e b
+     */
+    public double subtract(double a, double b) {
         return a - b;
     }
 
-    // método de Multiplicação
-    private double multiply(double a, double b) {
+    /**
+     * Multiplica dois números.
+     *
+     * @param a primeiro número
+     * @param b segundo número
+     * @return multiplicação de a e b
+     */
+    public double multiply(double a, double b) {
         return a * b;
     }
 
-    // método de Divisão
-    private double divide(double a, double b) {
-        if (b == 0)
+    /**
+     * Divide dois números.
+     *
+     * @param a primeiro número
+     * @param b segundo número
+     * @return divisão de a e b
+     * @throws IllegalArgumentException se dividir por 0
+     */
+    public double divide(double a, double b) {
+        if (b == 0) {
             throw new IllegalArgumentException("Division by zero");
+        }
         return a / b;
     }
 
-    // método de Potência
-    private double power(double base, double expoent) {
+    // ------------------- Operações Algébricas -------------------
+
+    /**
+     * Calcula a potência de uma base elevada a um expoente.
+     *
+     * @param base    a base da potência
+     * @param expoent o expoente
+     * @return o resultado de base elevado ao expoente
+     */
+    public double power(double base, double expoent) {
         return Math.pow(base, expoent);
     }
 
-    // método de Raiz Quadrada
-    private double squareRoot(double a) {
-        if (a < 0)
+    /**
+     * Calcula a raiz quadrada de um número.
+     *
+     * @param a o número de entrada
+     * @return a raiz quadrada de a
+     * @throws IllegalArgumentException se a for negativo
+     */
+    public double squareRoot(double a) {
+        if (a < 0) {
             throw new IllegalArgumentException("Negative Number");
+        }
         return Math.sqrt(a);
     }
 
-    // método de Log
-    private double log(double a) {
-        if (a <= 0)
+    // ------------------- Operações Exponenciais -------------------
+
+    /**
+     * Calcula o logaritmo um número.
+     *
+     * @param a o número de entrada
+     * @return o logaritmo de a
+     * @throws IllegalArgumentException se a for menor ou igual a 0
+     */
+    public double log(double a) {
+        if (a <= 0) {
             throw new IllegalArgumentException("Log of non-positive number");
+        }
         return Math.log(a);
     }
 
-    // método de Seno
-    private double sin(double degrees) {
+    // ------------------- Operações Trigonométricas -------------------
+
+    /**
+     * Calcula o seno de um ângulo em graus.
+     *
+     * @param degrees ângulo em graus
+     * @return o seno do ângulo
+     */
+    public double sin(double degrees) {
         return Math.sin(Math.toRadians(degrees));
     }
 
-    // método de Cos
-    private double cos(double degrees) {
+    /**
+     * Calcula o cosseno de um ângulo em graus.
+     *
+     * @param degrees o ângulo em graus
+     * @return o cosseno do ângulo
+     */
+    public double cos(double degrees) {
         return Math.cos(Math.toRadians(degrees));
-    }
-
-    // constructor
-    public ScientificCalculator(double a, double b, double degrees, double base, double expoent) {
-        this.add(a, b);
-        this.subtract(a, b);
-        this.multiply(a, b);
-        this.divide(a, b);
-        this.power(base, expoent);
-        this.squareRoot(a);
-        this.log(a);
-        this.sin(degrees);
-        this.cos(degrees);
     }
 
 }
